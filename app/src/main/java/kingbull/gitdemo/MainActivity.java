1976c620e5f62cdf8c19cfe7f8536f30a83da5da
Kailash Dabhi <kailash09dabhi@gmail.com>
2016-08-22 17:03:22 +0530

    MainActivity

---
+++ app/src/main/java/kingbull/gitdemo/MainActivity.java
@@ -0,0 +1,11 @@
+package kingbull.gitdemo;
+
+import android.app.Activity;
+
+/**
+ * Created by Kailash Dabhi on 22-08-2016.
+ * You can contact us at kailash09dabhi@gmail.com OR on skype(kailash.09)
+ * Copyright (c) 2016 Kingbull Technology. All rights reserved.
+ */
+public class MainActivity extends Activity {
+}
